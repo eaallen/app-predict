6a2a9d1ae7105a697d790e28b730e9e6ee53b230
eaallen <elijah.a.allen@gmail.com>
2020-04-20 11:10:00 -0600

    path m8

--- ArcticApi/settings.py
+++ ArcticApi/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'HOST': 'tcp:kandykane.database.windows.net',
         'PORT': '1433',
         'OPTIONS': {
-            'driver': 'ODBC Driver 13 for SQL Server',
+            'driver': "ODBC Driver 17 for SQL Server",
             'MARS_Connection': 'True',
         }
     }
