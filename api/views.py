72584716e53e64b2558d48795dc8ca8d9b33b4b3
eaallen <elijah.a.allen@gmail.com>
2020-04-20 16:49:16 -0600

    done son

--- api/views.py
+++ api/views.py
@@ -17,44 +17,58 @@ import requests
 class AzureML(APIView):
     @csrf_exempt
     def post(self, request, format=None):
-        print('----NMMMM',request.body)
-        
-        url = 'https://ussouthcentral.services.azureml.net/workspaces/151730822d384e008f146bde06c76aaa/services/14fd15e525ba4c7bb74f0565d5d167c5/execute?api-version=2.0&details=true'
-        headers = {'Content-Type': 'application/json','Authorization':'Bearer vEDXpoF6UlR2w7/kJpPDQkR4BUE+1sLPCUDpYPoE2epXJm+gb93XRsDwa/972qT4wrx04P617W33H21GivQFGQ=='}
+        print('----NMMMM',request.data)
+        data=request.data
+        # print('+++++++++++++++++++', request.body[0])
+        url = 'https://ussouthcentral.services.azureml.net/workspaces/8a1c3ee5c1c34dd6bf7f601ac1718b3d/services/1fedfe88788b419f8ec10f5e4ee93615/execute?api-version=2.0&details=true'
+        headers = {'Content-Type': 'application/json','Authorization':'Bearer 2xSFT5p90PXdI/A8JTavS49ieDBB4bWPaY6D9vGWs3W7YkoG7/QCQDwAZlN2ziEcY00mV0Mcx3/354Juh1QbVQ=='}
         payload = {"Inputs": {
                     "input1": {
-                        "ColumnNames": [
-                            "Marital Status",
-                            "Gender",
-                            "Income",
-                            "Children",
-                            "Cars",
-                            "Age",
-                            "Education",
-                            "Occupation",
-                            "Home Owner",
-                            "Commute Distance",
-                            "Region"
-                        ],
-                        "Values": [
-                            [
-                                "Married",
-                                "Male",
-                                "10000",
-                                "0",
-                                "2",
-                                "18",
-                                "graduate",
-                                "managment",
-                                "yes",
-                                "0-1 miles",
-                                "Europe"
-                            ]
+                    "ColumnNames": [
+                        "id",
+                        "track_name",
+                        "size_bytes",
+                        "currency",
+                        "price",
+                        "rating_count_tot",
+                        "rating_count_ver",
+                        "user_rating",
+                        "user_rating_ver",
+                        "ver",
+                        "cont_rating",
+                        "prime_genre",
+                        "sup_devices_num",
+                        "ipadSc_urls_num",
+                        "lang_num",
+                        "vpp_lic",
+                        "app_desc"
+                    ],
+                    "Values": [
+                        [
+                        '23453',
+                        data[0],
+                        data[1],
+                        "USD",
+                        data[2],
+                        '2974676',
+                        '212',
+                        '3.5',
+                        '3.5',
+                        '95',
+                        data[6],
+                        data[7],
+                        data[4],
+                        '1',
+                        data[5],
+                        data[8],
+                        data[3]
                         ]
+                    ]
                     }
                 },
                 "GlobalParameters": {}
             }
+        print('00000____>', payload)
         r = requests.post(url, headers=headers, data=json.dumps(payload))
         print('from the azure studio--------->',r.json())
         return Response(r.text)
